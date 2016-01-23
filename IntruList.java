48e32eb07547e6538f481906993e89278d046364
Paperhorse <paperhorse@fastmail.com>
2016-01-24 10:47:48 +1100

    added some more methods to IntruList including a sort routine

--- IntruList.java
+++ IntruList.java
@@ -1,18 +1,20 @@
+import java.util.Comparator;
 
 public abstract class IntruList<T> {
     T head;
     T tail;
     long count;
 
-    public abstract T getNext(T e);
-    public abstract void setNext(T e,T next);
-    public abstract T getPrevious(T e);
-    public abstract void setPrevious(T e,T previous);
+    public abstract T getNextLink(T e);
+    public abstract void setNextLink(T e,T next);
+    public abstract T getPreviousLink(T e);
+    public abstract void setPreviousLink(T e,T previous);
     
     public void push(T e) {
-        setNext(e,head);
-        if (head!=null) setPrevious(head,e);
-        setPrevious(e,null);
+        setNextLink(e,head);
+        if (head!=null) setPreviousLink(head,e);
+        else tail=e;
+        setPreviousLink(e,null);
         head=e;
         count++;
     }
@@ -21,13 +23,90 @@ public abstract class IntruList<T> {
         T e;
         e=head;
         if (e!=null) {
-            head=getNext(e);
+            head=getNextLink(e);
             if (head!=null)
-                setPrevious(head,null);
+                setPreviousLink(head,null);
+            else
+                tail=null;
             count--;
         }
         return e;
     }
     
+    public void insertAfter(T existingElement, T newElement) {
+        T n=getNextLink(existingElement);
+        setNextLink(existingElement, newElement);
+        setPreviousLink(newElement, existingElement);
+        if (n!=null) {
+            setNextLink(newElement, n);
+            setPreviousLink(n, newElement);
+        } else {
+            tail=newElement;
+            setNextLink(newElement, null);
+        }
+        count++;
+    }
+    
+    public void append(T e) {
+        setPreviousLink(e, tail);
+        setNextLink(tail, e);
+        setNextLink(e, null);
+        if (tail==null) head=e;
+        tail=e;
+        count++;
+    }
+    
+    public long length() {return count;}
+    
+    private T doubleLink(T q) {
+        T p=null;
+        while (q!=null) {
+            setPreviousLink(q,p);
+            p=q;
+            q=getNextLink(q);
+        }
+        return p;
+    }
+    
+    private T merge(T a, T b, Comparator<T> cmp) {
+        T hd=null, tl=null;
+        while (a!=null && b!=null) {
+            if (cmp.compare(a,b)<=0) {
+                if (tl!=null) setNextLink(tl, a);
+                else hd=a;
+                tl=a;
+                a=getNextLink(a); 
+            } else {
+                if (tl!=null) setNextLink(tl, b);
+                else hd=b;
+                tl=b;
+                b=getNextLink(b); 
+            }
+        }
+        if (a==null) a=b;
+        if (tl==null) return a;
+        else setNextLink(tl,a);
+        return hd;
+    }
+    
+    private T sort(long cnt,Comparator<T> cmp) {
+        T a, b;
+        if (cnt<=0) return null;
+        if (cnt==1) {
+            a=head;
+            head=getNextLink(a);
+            setNextLink(a,null);
+            return a;
+        } else {
+            a=sort(cnt/2,cmp);
+            b=sort(cnt-cnt/2,cmp);
+            return merge(a,b,cmp);
+        }
+    }
+    
+    public void sort(Comparator<T> cmp) {
+        head=sort(count, cmp);
+        tail=doubleLink(head);
+    }
     
 }
--- Test1.java
+++ Test1.java
@@ -1,4 +1,5 @@
 
+import java.util.Comparator;
 
 public class Test1 {
     
@@ -20,16 +21,24 @@ public class Test1 {
     
     public static void main(String[] args) {
         IntruList<SubTest> hd=new IntruList<SubTest>() {
-            public  SubTest getNext(SubTest e) {return e.next;}
-            public  void setNext(SubTest e,SubTest next) {e.next=next;}
-            public  SubTest getPrevious(SubTest e) {return e.previous;}
-            public  void setPrevious(SubTest e,SubTest previous) 
+            public  SubTest getNextLink(SubTest e) {return e.next;}
+            public  void setNextLink(SubTest e,SubTest next) {e.next=next;}
+            public  SubTest getPreviousLink(SubTest e) {return e.previous;}
+            public  void setPreviousLink(SubTest e,SubTest previous) 
                             {e.previous=previous;}
         };
         hd.push(new SubTest("Olivia Dunham"));
         hd.push(new SubTest("Astrid Farnsworth"));
         hd.push(new SubTest("Peter Bishop"));
         hd.push(new SubTest("Walter Bishop"));
+        hd.sort(new Comparator<SubTest>() {
+            public int compare(SubTest o1,SubTest o2) {
+                return o1.customer.compareTo(o2.customer);
+            }
+            public boolean equals(SubTest o1, SubTest o2) {
+                return o1.customer.equals(o2.customer);
+            }
+        });
         SubTest s;
         while ((s=(SubTest)hd.pop())!=null) System.out.println(s);
     }
